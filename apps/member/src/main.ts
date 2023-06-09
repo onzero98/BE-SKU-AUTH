df2474b67bf14747155e149c3381cc5b98d4c6b9
In Hwan KIM <dev_210@naver.com>
2023-06-09 18:32:45 +0900

    Delete apps/member directory

--- apps/member/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MemberModule } from './member.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MemberModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/member/src/member.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class MemberService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
